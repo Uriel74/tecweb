a8211e8102984975ff7ab64d1d54f5b3948cde1c
Uriel74 <urielvalero4@gmail.com>
2023-02-22 22:43:12 -0600

    p05

---
+++ practicas/p05/get_productos_xhtml.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Práctica 5</title>
+  </head>
+  <body>
+  <?php
+    header("Content-Type: application/json; charset=utf-8"); 
+    $data = array();
+
+	if(isset($_GET['tope']))
+    {
+		$tope = $_GET['tope'];
+    }
+    else
+    {
+        die('Parámetro "tope" no detectado...');
+    }
+
+	if (!empty($tope))
+	{
+		/** SE CREA EL OBJETO DE CONEXION */
+		@$link = new mysqli('localhost', 'root', '123456', 'marketzone');
+        /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+
+		/** comprobar la conexión */
+		if ($link->connect_errno) 
+		{
+			die('Falló la conexión: '.$link->connect_error.'<br/>');
+			//exit();
+		}
+
+		/** Crear una tabla que no devuelve un conjunto de resultados */
+		if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope") ) 
+		{
+            /** Se extraen las tuplas obtenidas de la consulta */
+			$row = $result->fetch_all(MYSQLI_ASSOC);
+
+            /** Se crea un arreglo con la estructura deseada */
+            foreach($row as $num => $registro) {            // Se recorren tuplas
+                foreach($registro as $key => $value) {      // Se recorren campos
+                    $data[$num][$key] = utf8_encode($value);
+                }
+            }
+
+			/** útil para liberar memoria asociada a un resultado con demasiada información */
+			$result->free();
+		}
+
+		$link->close();
+
+        /** Se devuelven los datos en formato JSON */
+        echo json_encode($data, JSON_PRETTY_PRINT);
+	}
+	?>
+  </div>
+<p>
+    <a href="http://validator.w3.org/check?uri=referer"><img
+      src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+  </p>
+        </body>
+        </html>
