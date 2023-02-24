d859de536b85704d9fabab3585a08af882227479
Uriel74 <urielvalero4@gmail.com>
2023-02-23 20:25:25 -0600

    p05

--- practicas/p05/get_productos_xhtml.php
+++ practicas/p05/get_productos_xhtml.php
@@ -1,62 +1,78 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <title>Práctica 5</title>
-  </head>
-  <body>
-  <?php
-    header("Content-Type: application/json; charset=utf-8"); 
-    $data = array();
-
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+	<?php
 	if(isset($_GET['tope']))
-    {
 		$tope = $_GET['tope'];
-    }
-    else
-    {
-        die('Parámetro "tope" no detectado...');
-    }
+if (!empty($tope))
+{
+	/** SE CREA EL OBJETO DE CONEXION */
+	@$link = new mysqli('localhost', 'root', '123456', 'marketzone');	
 
-	if (!empty($tope))
+	/** comprobar la conexión */
+	if ($link->connect_errno) 
 	{
-		/** SE CREA EL OBJETO DE CONEXION */
-		@$link = new mysqli('localhost', 'root', '123456', 'marketzone');
-        /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
-
-		/** comprobar la conexión */
-		if ($link->connect_errno) 
-		{
-			die('Falló la conexión: '.$link->connect_error.'<br/>');
-			//exit();
-		}
-
-		/** Crear una tabla que no devuelve un conjunto de resultados */
-		if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope") ) 
-		{
-            /** Se extraen las tuplas obtenidas de la consulta */
-			$row = $result->fetch_all(MYSQLI_ASSOC);
-
-            /** Se crea un arreglo con la estructura deseada */
-            foreach($row as $num => $registro) {            // Se recorren tuplas
-                foreach($registro as $key => $value) {      // Se recorren campos
-                    $data[$num][$key] = utf8_encode($value);
-                }
-            }
-
-			/** útil para liberar memoria asociada a un resultado con demasiada información */
-			$result->free();
-		}
-
-		$link->close();
-
-        /** Se devuelven los datos en formato JSON */
-        echo json_encode($data, JSON_PRETTY_PRINT);
+		die('Falló la conexión: '.$link->connect_error.'<br/>');
+		    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
 	}
-	?>
-  </div>
-<p>
-    <a href="http://validator.w3.org/check?uri=referer"><img
-      src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
-  </p>
-        </body>
-        </html>
+
+	/** Crear una tabla que no devuelve un conjunto de resultados */
+	if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= '{$tope}'") ) 
+	{
+		$rows = $result->fetch_all(MYSQLI_ASSOC);
+		/** útil para liberar memoria asociada a un resultado con demasiada información */
+		$result->free();
+	}
+
+	$link->close();
+}
+?>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Productos</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<h3>PRODUCTOS</h3>
+
+	<br/>
+	
+	<?php if( isset($rows) ) : ?>
+
+		<table class="table">
+			<thead class="thead-dark">
+				<tr>
+				<th scope="col">#</th>
+				<th scope="col">Nombre</th>
+				<th scope="col">Marca</th>
+				<th scope="col">Modelo</th>
+				<th scope="col">Precio</th>
+				<th scope="col">Unidades</th>
+				<th scope="col">Detalles</th>
+				<th scope="col">Imagen</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach($rows as $row) : ?>
+				<tr>
+					<th scope="row"><?= $row['id'] ?></th>
+					<td><?= $row['nombre'] ?></td>
+					<td><?= $row['marca'] ?></td>
+					<td><?= $row['modelo'] ?></td>
+					<td><?= $row['precio'] ?></td>
+					<td><?= $row['unidades'] ?></td>
+					<td><?= utf8_encode($row['detalles']) ?></td>
+					<td><img src=<?= $row['imagen'] ?> ></td>
+				</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+
+	<?php elseif(!empty($tope)) : ?>
+
+		 <script>
+            alert('No hay productos con una cantidad de unidades menor o igual al tope especificado');
+         </script>
+
+	<?php endif; ?>
+</body>
+</html> 
